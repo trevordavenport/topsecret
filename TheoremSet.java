bd6e9a448fdce1aaf7749f68ba41345393da63ab
dahau5 <sam.hausman@gmail.com>
2013-07-25 19:41:04 -0700

    Update TheoremSet.java
    Implemented TheoremSet
    Pending debugging & testing

--- TheoremSet.java
+++ TheoremSet.java
@@ -1,9 +1,18 @@
+import java.util.*;
+
+
 public class TheoremSet {
 
-  public TheoremSet ( ) {
+	HashMap<String, Expression> myThms;
+
+	public TheoremSet ( ) {
+		myThms = new HashMap<String, Expression>();
 	}
 
 	public Expression put (String s, Expression e) {
-		return null;
+
+		//Sets the value at Key(s) in myThems to Expression(e) 
+		//and returns the old value at S or null if empty.
+		return myThms.put(s,e);
 	}
 }
