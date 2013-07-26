0cf0bf7ee08e3c64fff0c9fb63cbc1bb9a61148d
vwb <vwb@berkeley.edu>
2013-07-25 23:35:06 -0700

    Update Proof.java

--- Proof.java
+++ Proof.java
@@ -1,15 +1,20 @@
 import java.util.*;
 
 public class Proof {
-
+	
+	LineNumber lastLineNumber;
+	
   public Proof (TheoremSet theorems) {
 	}
 
 	public LineNumber nextLineNumber ( ) {
-		return null;
+		LineNumber valueholder = nextline_helper(hashmap.get(lastLineNumber).first(), hashmap.get(lastLineNumber).last());
+		lastLineNumber = valueholder;
+		return valueholder;
 	}
 
 	public void extendProof (String x) throws IllegalLineException, IllegalInferenceException {
+	//add the parsed expression to the hashmap at the key of "lastLineNumber"
 	}
 
 	public String toString ( ) {
